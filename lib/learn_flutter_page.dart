273a2aa48b08f5519f0a4cceb54031eaf79b7fb5
tmukadam <t.mukadam@techgrowthkw.com>
2023-03-23 12:52:45 +0300

    first commit

---
+++ lib/learn_flutter_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn Flutter'),
+      ),
+    );
+  }
+}
