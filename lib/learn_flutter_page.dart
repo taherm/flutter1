ee865180ee99cf2279d191534beb75b15ba49fec
tmukadam <t.mukadam@techgrowthkw.com>
2023-03-26 11:02:11 +0300

    new changes

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -14,6 +14,58 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
       appBar: AppBar(
         title: const Text('Learn Flutter'),
       ),
+      body: Column(
+        children: [
+          Image.asset('images/es.jpeg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.amber,
+          ),
+          Container(
+            color: Colors.amber,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'This is a text widget',
+                style: TextStyle(color: Colors.cyan),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint('button pressed elevated button');
+            },
+            child: const Text('Elevated Button'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint('button pressed Outlined button');
+            },
+            child: const Text('Outlined Button'),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint('button pressed Text button');
+            },
+            child: const Text('Text Button'),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.local_fire_department,
+                color: Colors.blue,
+              ),
+              Text('hello row'),
+              Icon(Icons.local_fire_department),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
